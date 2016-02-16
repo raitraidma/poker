e6cb26db4d76c0f7587052f2a441ab05866505a6
eraitra <rait.raidma@ericsson.com>
2016-02-16 02:47:21 +0200

    Refactored. Added tests.

--- src/main/java/com/nortal/game/poker/hand/StandardPokerHandEvaluator.java
+++ src/main/java/com/nortal/game/poker/hand/StandardPokerHandEvaluator.java
@@ -15,14 +15,14 @@ import static com.nortal.game.poker.card.Rank.TWO;
 
 public class StandardPokerHandEvaluator implements HandEvaluator {
   public static final long HIGH_CARD_BASE = 0;
-  public static final long ONE_PAIR_BASE = HIGH_CARD_BASE + 31744;
+  public static final long ONE_PAIR_BASE = HIGH_CARD_BASE + 73728;
   public static final long TWO_PAIRS_BASE = ONE_PAIR_BASE + 1414336;
-  public static final long THREE_OF_A_KIND_BASE = TWO_PAIRS_BASE + 141312;
+  public static final long THREE_OF_A_KIND_BASE = TWO_PAIRS_BASE + 14134096;
   public static final long STRAIGHT_BASE = THREE_OF_A_KIND_BASE + 1416384;
   public static final long FLUSH_BASE = STRAIGHT_BASE + 14;
   public static final long FULL_HOUSE_BASE = FLUSH_BASE + 31744;
   public static final long FOUR_OF_A_KIND_BASE = FULL_HOUSE_BASE + 1413;
-  public static final long STRAIGHT_FLUSH_BASE = FOUR_OF_A_KIND_BASE + 1413;
+  public static final long STRAIGHT_FLUSH_BASE = FOUR_OF_A_KIND_BASE + 148192;
 
   @Override
   public long evaluate(Hand hand) {
@@ -43,32 +43,16 @@ public class StandardPokerHandEvaluator implements HandEvaluator {
   }
 
   protected long getStraightFlushValue(Hand hand) {
-    Collections.sort(hand);
-    for (int i = 0; i < hand.size()-1; i++) {
-      Card currentCard = hand.get(i);
-      Card nextCard = hand.get(i + 1);
-
-      if (!currentCard.getSuit().equals(nextCard.getSuit())) break;
-      if (currentCard.getRank().getRankValue() != nextCard.getRank().getRankValue() - 1) {
-        if (hand.get(0).getRank().equals(TWO) && currentCard.getRank().equals(FIVE)
-            && nextCard.getRank().equals(ACE) && i == hand.size() - 2) {
-          return STRAIGHT_FLUSH_BASE + FIVE.getRankValue();
-        } else {
-          break;
-        }
-      }
-      if (i == hand.size()-2) return STRAIGHT_FLUSH_BASE + nextCard.getRank().getRankValue();
-    }
-    return 0;
+    long value = calculateStraightValue(hand, true);
+    return value == 0 ? 0 : STRAIGHT_FLUSH_BASE + value;
   }
 
   protected long getFourOfAKindValue(Hand hand) {
     List<Rank> ranksInFour = getRanksInGroupOfSize(hand, 4);
     if (ranksInFour.size() != 1) return 0;
 
-    long value = ranksInFour.get(0).getRankValue() * 100;
-    value += hand.stream().filter(card -> !card.getRank().equals(ranksInFour.get(0)))
-        .findFirst().get().getRank().getRankValue();
+    long value = ranksInFour.get(0).getRankValue() * 10000;
+    value += calculateKickersValue(hand, ranksInFour);
     return FOUR_OF_A_KIND_BASE + value;
   }
 
@@ -90,22 +74,8 @@ public class StandardPokerHandEvaluator implements HandEvaluator {
   }
 
   protected long getStraightValue(Hand hand) {
-    Collections.sort(hand);
-    for (int i = 0; i < hand.size()-1; i++) {
-      Card currentCard = hand.get(i);
-      Card nextCard = hand.get(i + 1);
-
-      if (currentCard.getRank().getRankValue() != nextCard.getRank().getRankValue() - 1) {
-        if (hand.get(0).getRank().equals(TWO) && currentCard.getRank().equals(FIVE)
-            && nextCard.getRank().equals(ACE) && i == hand.size() - 2) {
-          return STRAIGHT_BASE + FIVE.getRankValue();
-        } else {
-          break;
-        }
-      }
-      if (i == hand.size()-2) return STRAIGHT_BASE + nextCard.getRank().getRankValue();
-    }
-    return 0;
+    long value = calculateStraightValue(hand, false);
+    return value == 0 ? 0 : STRAIGHT_BASE + value;
   }
 
   protected long getThreeOfAKindValue(Hand hand) {
@@ -113,9 +83,7 @@ public class StandardPokerHandEvaluator implements HandEvaluator {
     if (ranksInTriple.size() != 1) return 0;
 
     long value = ranksInTriple.get(0).getRankValue() * 100000;
-    value += (long)hand.stream().filter(card -> !card.getRank().equals(ranksInTriple.get(0)))
-        .mapToDouble(card -> Math.pow(2, card.getRank().getRankValue()))
-        .sum();
+    value += calculateKickersValue(hand, ranksInTriple);
     return value + THREE_OF_A_KIND_BASE;
   }
 
@@ -125,11 +93,9 @@ public class StandardPokerHandEvaluator implements HandEvaluator {
     Collections.sort(ranksInPair);
     Collections.reverse(ranksInPair);
 
-    long value = ranksInPair.get(0).getRankValue() * 10000;
-    value += ranksInPair.get(1).getRankValue() * 100;
-
-    value += (long)hand.stream().filter(card -> !ranksInPair.contains(card.getRank()))
-        .findFirst().get().getRank().getRankValue();
+    long value = ranksInPair.get(0).getRankValue() * 1000000;
+    value += ranksInPair.get(1).getRankValue() * 10000;
+    value += calculateKickersValue(hand, ranksInPair);
     return value + TWO_PAIRS_BASE;
   }
 
@@ -138,31 +104,63 @@ public class StandardPokerHandEvaluator implements HandEvaluator {
     if (ranksInPair.size() != 1) return 0;
 
     long value = ranksInPair.get(0).getRankValue() * 100000;
-    value += (long)hand.stream().filter(card -> !card.getRank().equals(ranksInPair.get(0)))
+    value += calculateKickersValue(hand, ranksInPair);
+    return value + ONE_PAIR_BASE;
+  }
+
+  protected long calculateKickersValue(Hand hand, List<Rank> ranksInGroup) {
+    return (long)hand.stream().filter(card -> !ranksInGroup.contains(card.getRank()))
         .mapToDouble(card -> Math.pow(2, card.getRank().getRankValue()))
         .sum();
-    return value + ONE_PAIR_BASE;
+  }
+
+  protected boolean cardsDoNotHaveConsecutiveRanks(Card currentCard, Card nextCard) {
+    return currentCard.getRank().getRankValue() != nextCard.getRank().getRankValue() - 1;
+  }
+
+  protected boolean isFiveHighStraight(Card firstCard, Card lastButOneCard, Card lastCard) {
+    return firstCard.getRank().equals(TWO)
+        && lastButOneCard.getRank().equals(FIVE)
+        && lastCard.getRank().equals(ACE);
+  }
+
+  protected long getHighCardValue(Hand hand) {
+    return calculateKickersValue(hand, Collections.<Rank>emptyList());
   }
 
   protected Map<Rank, List<Card>> groupCardsByRank(Hand hand) {
     return hand.stream().collect(Collectors.groupingBy(Card::getRank));
   }
 
-  protected long getHighCardValue(Hand hand) {
-    return (long)hand.stream()
-        .mapToDouble(card -> Math.pow(2, card.getRank().getRankValue()))
-        .sum();
-  }
-
   protected long getNewEvaluationValue(long evaluationValue, long currentCategoryValue) {
     return currentCategoryValue > 0 ? currentCategoryValue : evaluationValue;
   }
 
-  private List<Rank> getRanksInGroupOfSize(Hand hand, int groupSize) {
+  protected List<Rank> getRanksInGroupOfSize(Hand hand, int groupSize) {
     Map<Rank, List<Card>> cardsGroupedByRank = groupCardsByRank(hand);
     List<Rank> ranks = cardsGroupedByRank.keySet().stream()
         .filter(rank -> cardsGroupedByRank.get(rank).size() == groupSize)
         .collect(Collectors.toList());
     return ranks;
+  }
+
+  protected long calculateStraightValue(Hand hand, boolean isFlush) {
+    Collections.sort(hand);
+    for (int i = 0; i < hand.size()-1; i++) {
+      Card currentCard = hand.get(i);
+      Card nextCard = hand.get(i + 1);
+
+      if (isFlush && !currentCard.getSuit().equals(nextCard.getSuit())) break;
+      boolean isComparingLastCardInHand = i == hand.size() - 2;
+      if (cardsDoNotHaveConsecutiveRanks(currentCard, nextCard)) {
+        if (isFiveHighStraight(hand.get(0), currentCard, nextCard) && isComparingLastCardInHand) {
+          return FIVE.getRankValue();
+        } else {
+          break;
+        }
+      }
+      if (isComparingLastCardInHand) return nextCard.getRank().getRankValue();
+    }
+    return 0;
   }
 }
--- src/test/java/com/nortal/game/poker/hand/StandardPokerHandEvaluatorTest.java
+++ src/test/java/com/nortal/game/poker/hand/StandardPokerHandEvaluatorTest.java
@@ -5,18 +5,18 @@ import com.nortal.game.poker.card.Rank;
 import com.nortal.game.poker.card.Suit;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import static com.nortal.game.poker.card.Rank.*;
 import static com.nortal.game.poker.card.Suit.*;
 import static com.nortal.game.poker.hand.StandardPokerHandEvaluator.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class StandardPokerHandEvaluatorTest {
-
-  @Test
-  public void getNewEvaluationValue() throws Exception {
-    assertEquals(10, new StandardPokerHandEvaluator().getNewEvaluationValue(10, 0));
-    assertEquals(20, new StandardPokerHandEvaluator().getNewEvaluationValue(10, 20));
-  }
 
   @Test
   public void getHighCardValue() throws Exception {
@@ -33,7 +33,7 @@ public class StandardPokerHandEvaluatorTest {
   @Test
   public void getTwoPairsValue() throws Exception {
     Hand hand = createHand(card(ACE, SPADE), card(ACE, SPADE), card(KING, SPADE), card(KING, SPADE), card(QUEEN, SPADE));
-    assertEquals(141312 + TWO_PAIRS_BASE, new StandardPokerHandEvaluator().getTwoPairsValue(hand));
+    assertEquals(14134096 + TWO_PAIRS_BASE, new StandardPokerHandEvaluator().getTwoPairsValue(hand));
   }
 
   @Test
@@ -81,7 +81,7 @@ public class StandardPokerHandEvaluatorTest {
   @Test
   public void getFourOfAKindValue() throws Exception {
     Hand hand = createHand(card(ACE, SPADE), card(ACE, SPADE), card(ACE, SPADE), card(ACE, SPADE), card(KING, SPADE));
-    assertEquals(1413 + FOUR_OF_A_KIND_BASE, new StandardPokerHandEvaluator().getFourOfAKindValue(hand));
+    assertEquals(148192 + FOUR_OF_A_KIND_BASE, new StandardPokerHandEvaluator().getFourOfAKindValue(hand));
   }
 
   @Test
@@ -100,6 +100,116 @@ public class StandardPokerHandEvaluatorTest {
   public void getStraightFlush5HighValueWithWrongSuit() throws Exception {
     Hand hand = createHand(card(FIVE, SPADE), card(FOUR, SPADE), card(THREE, SPADE), card(TWO, SPADE), card(ACE, DIAMOND));
     assertEquals(0, new StandardPokerHandEvaluator().getStraightFlushValue(hand));
+  }
+
+  @Test
+  public void getNewEvaluationValue() throws Exception {
+    assertEquals(10, new StandardPokerHandEvaluator().getNewEvaluationValue(10, 0));
+    assertEquals(20, new StandardPokerHandEvaluator().getNewEvaluationValue(10, 20));
+  }
+
+  @Test
+  public void calculateKickersValue() throws Exception {
+    Hand hand = createHand(card(ACE, SPADE), card(KING, SPADE), card(QUEEN, SPADE), card(JACK, SPADE), card(TEN, SPADE));
+    assertEquals(31744, new StandardPokerHandEvaluator().calculateKickersValue(hand, new ArrayList<>()));
+  }
+
+  @Test
+  public void getRanksInGroupOfSize() throws Exception {
+    Hand hand = createHand(card(FIVE, SPADE), card(FIVE, DIAMOND), card(THREE, SPADE), card(THREE, DIAMOND), card(ACE, DIAMOND));
+    List<Rank> ranksInGroupOfSize2 = new StandardPokerHandEvaluator().getRanksInGroupOfSize(hand, 2);
+    assertEquals(2, ranksInGroupOfSize2.size());
+    assertTrue(ranksInGroupOfSize2.contains(FIVE));
+    assertTrue(ranksInGroupOfSize2.contains(THREE));
+  }
+
+  @Test
+  public void groupCardsByRank() throws Exception {
+    Hand hand = createHand(card(FIVE, SPADE), card(FIVE, DIAMOND), card(THREE, SPADE), card(THREE, DIAMOND), card(ACE, DIAMOND));
+    Map<Rank, List<Card>> cardsByRank = new StandardPokerHandEvaluator().groupCardsByRank(hand);
+    assertEquals(3, cardsByRank.size());
+    assertEquals(2, cardsByRank.get(FIVE).size());
+    assertEquals(2, cardsByRank.get(THREE).size());
+    assertEquals(1, cardsByRank.get(ACE).size());
+  }
+
+  @Test
+  public void cardsDoNotHaveConsecutiveRanks() throws Exception {
+    StandardPokerHandEvaluator handEvaluator = new StandardPokerHandEvaluator();
+    assertTrue(handEvaluator.cardsDoNotHaveConsecutiveRanks(card(FIVE, SPADE), card(SEVEN, SPADE)));
+    assertTrue(handEvaluator.cardsDoNotHaveConsecutiveRanks(card(SIX, SPADE), card(FIVE, SPADE)));
+    assertFalse(handEvaluator.cardsDoNotHaveConsecutiveRanks(card(SIX, SPADE), card(SEVEN, SPADE)));
+  }
+
+  @Test
+  public void isFiveHighStraight() throws Exception {
+    StandardPokerHandEvaluator handEvaluator = new StandardPokerHandEvaluator();
+    assertTrue(handEvaluator.isFiveHighStraight(card(TWO, SPADE), card(FIVE, SPADE), card(ACE, SPADE)));
+    assertFalse(handEvaluator.isFiveHighStraight(card(ACE, SPADE), card(FIVE, SPADE), card(TWO, SPADE)));
+  }
+
+  @Test
+  public void onePairIsBetterThanHighCard() throws Exception {
+    StandardPokerHandEvaluator handEvaluator = new StandardPokerHandEvaluator();
+    Hand highCardHigh = createHand(card(KING, DIAMOND), card(ACE, DIAMOND), card(ACE, HEART), card(ACE, CLUB), card(ACE, SPADE));
+    Hand onePairLow = createHand(card(TWO, SPADE), card(TWO, DIAMOND), card(THREE, HEART), card(THREE, SPADE), card(THREE, CLUB));
+    assertTrue(handEvaluator.getHighCardValue(highCardHigh) < handEvaluator.getOnePairValue(onePairLow));
+  }
+
+  @Test
+  public void twoPairsIsBetterThanOnePair() throws Exception {
+    StandardPokerHandEvaluator handEvaluator = new StandardPokerHandEvaluator();
+    Hand onePairHigh = createHand(card(ACE, SPADE), card(ACE, DIAMOND), card(KING, HEART), card(KING, SPADE), card(KING, CLUB));
+    Hand twoPairsLow = createHand(card(TWO, DIAMOND), card(TWO, DIAMOND), card(THREE, HEART), card(THREE, CLUB), card(FOUR, SPADE));
+    assertTrue(handEvaluator.getOnePairValue(onePairHigh) < handEvaluator.getTwoPairsValue(twoPairsLow));
+  }
+
+  @Test
+  public void threeOfAKindIsBetterThanTwoPairs() throws Exception {
+    StandardPokerHandEvaluator handEvaluator = new StandardPokerHandEvaluator();
+    Hand twoPairsHigh = createHand(card(ACE, DIAMOND), card(ACE, DIAMOND), card(KING, HEART), card(KING, CLUB), card(QUEEN, SPADE));
+    Hand threeOfAKindLow = createHand(card(TWO, SPADE), card(TWO, DIAMOND), card(TWO, HEART), card(THREE, SPADE), card(THREE, CLUB));
+    assertTrue(handEvaluator.getTwoPairsValue(twoPairsHigh) < handEvaluator.getThreeOfAKindValue(threeOfAKindLow));
+  }
+
+  @Test
+  public void straightIsBetterThanThreeOfAKind() throws Exception {
+    StandardPokerHandEvaluator handEvaluator = new StandardPokerHandEvaluator();
+    Hand threeOfAKindHigh = createHand(card(ACE, SPADE), card(ACE, DIAMOND), card(ACE, HEART), card(KING, SPADE), card(KING, CLUB));
+    Hand straightLow = createHand(card(TWO, DIAMOND), card(THREE, DIAMOND), card(FOUR, HEART), card(FIVE, CLUB), card(ACE, SPADE));
+    assertTrue(handEvaluator.getThreeOfAKindValue(threeOfAKindHigh) < handEvaluator.getStraightValue(straightLow));
+  }
+
+  @Test
+  public void flushIsBetterThanStraight() throws Exception {
+    StandardPokerHandEvaluator handEvaluator = new StandardPokerHandEvaluator();
+    Hand straightHigh = createHand(card(ACE, DIAMOND), card(KING, DIAMOND), card(QUEEN, HEART), card(JACK, CLUB), card(TEN, SPADE));
+    Hand flushLow = createHand(card(TWO, SPADE), card(THREE, SPADE), card(FOUR, SPADE), card(FIVE, SPADE), card(SIX, SPADE));
+    assertTrue(handEvaluator.getStraightValue(straightHigh) < handEvaluator.getFlushValue(flushLow));
+  }
+
+  @Test
+  public void fullHouseIsBetterThanFlush() throws Exception {
+    StandardPokerHandEvaluator handEvaluator = new StandardPokerHandEvaluator();
+    Hand flushHigh = createHand(card(ACE, SPADE), card(KING, SPADE), card(QUEEN, SPADE), card(JACK, SPADE), card(TEN, SPADE));
+    Hand fullHouseLow = createHand(card(TWO, DIAMOND), card(TWO, DIAMOND), card(TWO, HEART), card(THREE, CLUB), card(THREE, SPADE));
+    assertTrue(handEvaluator.getFlushValue(flushHigh) < handEvaluator.getFullHouseValue(fullHouseLow));
+  }
+
+  @Test
+  public void fourOfAKindIsBetterThanFullHouse() throws Exception {
+    StandardPokerHandEvaluator handEvaluator = new StandardPokerHandEvaluator();
+    Hand fullHouseHigh = createHand(card(ACE, DIAMOND), card(ACE, DIAMOND), card(ACE, HEART), card(KING, CLUB), card(KING, SPADE));
+    Hand fourOfAKindLow = createHand(card(TWO, SPADE), card(TWO, SPADE), card(TWO, SPADE), card(TWO, SPADE), card(THREE, SPADE));
+    assertTrue(handEvaluator.getFullHouseValue(fullHouseHigh) < handEvaluator.getFourOfAKindValue(fourOfAKindLow));
+  }
+
+  @Test
+  public void straightFlushIsBetterThanFourOfAKind() throws Exception {
+    StandardPokerHandEvaluator handEvaluator = new StandardPokerHandEvaluator();
+    Hand fourOfAKindHigh = createHand(card(ACE, SPADE), card(ACE, SPADE), card(ACE, SPADE), card(ACE, SPADE), card(KING, SPADE));
+    Hand straightFlushLow = createHand(card(TWO, SPADE), card(THREE, SPADE), card(FOUR, SPADE), card(FIVE, SPADE), card(ACE, SPADE));
+    assertTrue(handEvaluator.getFourOfAKindValue(fourOfAKindHigh) < handEvaluator.getStraightFlushValue(straightFlushLow));
   }
 
   private Card card(Rank rank, Suit suit) {
